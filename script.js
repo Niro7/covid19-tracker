640f802dc39ef31f54a01224ce2fe09429bfe06b
K.Niroshani <niroshini.07.skk@gmail.com>
2020-07-13 22:13:45 +0530

    ADD: Map styles

---
+++ script.js
@@ -0,0 +1,10 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+    mapTypeControl: false,
+  });
+
+  styleSelectorControls();
+}
