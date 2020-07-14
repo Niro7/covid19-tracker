c49e5f95998261a7d01a7ddb8cec03871c72a553
K.Niroshani <niroshini.07.skk@gmail.com>
2020-07-14 11:03:47 +0530

    ADD: Table

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
+window.onload = () => {
+  getCountriesData();
+  //   getCountryData();
+};
+let coronaGlobalData;
 var map;
+const mapCenter = {
+  lat: 34.80746,
+  lng: -40.4796,
+};
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    center: mapCenter,
+    zoom: 3,
     mapTypeControl: false,
   });
 
   styleSelectorControls();
 }
+const getCountriesData = () => {
+  fetch("https://corona.lmao.ninja/v2/countries")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      coronaGlobalData = data;
+      showDataInTable(data);
+    });
+};
+const showDataInTable = (data) => {
+  var html = "";
+  data.forEach((country) => {
+    html += `
+        <tr>
+            <td>${country.country}</td>
+            <td>${numeral(country.cases).format("0,0")}</td>
+        </tr>
+        `;
+  });
+  document.getElementById("table-data").innerHTML = html;
+};
